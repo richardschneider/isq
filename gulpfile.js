bbf5bfc5ed92aec5cd8dcc6738bd4d9128e9bab0
Richard Schneider <makaretu@gmail.com>
2016-05-06 22:34:23 +1200

    building with gulp

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var mocha = require('gulp-spawn-mocha');
+var plugins = require('gulp-load-plugins')();
+var DEBUG = process.env.NODE_ENV === 'debug',
+    CI = process.env.CI === 'true';
+
+var paths = {
+  tests: ['./test/**/*.js', '!test/{temp,temp/**}'],
+  source: ['./lib/*.js', './bin/*']
+};
+paths.lint = paths.source.concat(paths.tests);
+var plumberConf = {};
+
+if (CI) {
+  plumberConf.errorHandler = function(err) {
+    throw err;
+  };
+}
+
+gulp.task('lint', function () {
+  return gulp.src(paths.lint)
+    .pipe(jshint())
+    .pipe(plugins.plumber(plumberConf))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('istanbul', function () {
+  return gulp.src(paths.tests, {read: false})
+    .pipe(mocha({
+      debugBrk: DEBUG,
+      R: CI ? 'spec' : 'nyan',
+      istanbul: !DEBUG
+    }));
+});
+
+gulp.task('bump', ['test'], function () {
+  var bumpType = plugins.util.env.type || 'patch'; // major.minor.patch
+
+  return gulp.src(['./package.json'])
+    .pipe(plugins.bump({ type: bumpType }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('test', ['lint', 'istanbul']);
+
+gulp.task('release', ['bump']);
+
+gulp.task('default', ['test']);
