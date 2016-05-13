0ab5ae487268864be25a7e3c40be2d8c0a5216e2
Richard Schneider <makaretu@gmail.com>
2016-05-13 13:59:31 +1200

    Add abs, pow and sqrt to named operators.

--- test/NumberExtensions.js
+++ test/NumberExtensions.js
@@ -14,12 +14,16 @@ describe('Number', () => {
     });
 
     it('should allow named operators', () => {
-        let a = 2, b = 3;
+        let a = 2, b = 3, c = -2;
         a.plus(b).should.equal(5);
         a.minus(b).should.equal(-1);
         a.times(b).should.equal(6);
         a.dividedBy(a).should.equal(1);
+        a.abs().should.equal(2);
+        c.abs().should.equal(2);
+        b.pow(2).should.equal(9);
+        (9).pow(0.5).should.equal(3);
+        (9).sqrt().should.equal(3);
     });
 
 });
-
