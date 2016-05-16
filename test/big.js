3b42c514a055ddc410a90aba87cfc1cb680fea30
Richard Schneider <makaretu@gmail.com>
2016-05-16 14:25:40 +1200

    Allow aliasing of method names.  Test big.js uncertainty propagation.

--- test/big.js
+++ test/big.js
@@ -36,6 +36,25 @@ describe('SI Number using big.js', () => {
         SI.Number('1.674 927 28(29) × 10⁻²⁷').uncertainty.eq(0.00000029e-27).should.be.true;
     });
 
+    it('should propagate uncertainty', () => {
+        let a = SI.Number('1.2(2)'),
+            b = SI.Number('1.3(3)'),
+            length = a.plus(b),
+            area = a.times(b);
+
+        length.should.be.instanceof(SI.config.UncertainNumber);
+        length.value.should.be.instanceof(Big);
+        length.uncertainty.should.be.instanceof(Big);
+        length.value.toFixed(1).should.be.equal('2.5');
+        length.uncertainty.toFixed(1).should.be.equal('0.4');
+
+        area.should.be.instanceof(SI.config.UncertainNumber);
+        area.value.should.be.instanceof(Big);
+        area.uncertainty.should.be.instanceof(Big);
+        area.value.toFixed(2).should.be.equal('1.56');
+        area.uncertainty.toFixed(1).should.be.equal('0.4');
+    });
+
     it('should be human readable', () => {
         SI.Number('1.2345(23)').toString().should.equal('1.2345 ± 0.0023');
     });
